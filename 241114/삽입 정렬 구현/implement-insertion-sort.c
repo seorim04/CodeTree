0cad17865cfbda66cc5a3111fcb3e02fe949ad5e
seorim04 <150996704+seorim04@users.noreply.github.com>
2024-11-14 21:11:32 +0900

    Title: 삽입 정렬 구현, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241114/삽입 정렬 구현/implement-insertion-sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#define MAX 100
+
+int n;
+int arr[MAX];
+
+void InsertSort()
+{
+    for(int i=1; i<n; i++)
+    {
+        int j=i-1;
+        int key=arr[i];
+        while(j>=0 && arr[j]>key)
+        {
+            arr[j+1]=arr[j];
+            j--
+        }
+        arr[j+1]=key;
+    }
+}
+int main() {
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",arr[i]);
+    }
+    InsertSort();
+
+    for(int i=0; i<n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+
+
+    return 0;
+}
