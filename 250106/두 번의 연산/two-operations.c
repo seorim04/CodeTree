cba9f1d0fd83601d8ce732ffbcfdd15336cb96b8
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-01-06 01:33:03 +0900

    Title: 두 번의 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250106/두 번의 연산/two-operations.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d", &a);
+
+    if(a%2!=0)
+    {
+        a+=3;
+    }
+    if(a%3==0)
+    {
+        a/=3;
+    }
+    printf("%d",a);
+    return 0;
+}
