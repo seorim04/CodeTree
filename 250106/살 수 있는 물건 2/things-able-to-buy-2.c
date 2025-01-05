85e1f963f55ed2aaf572e438f7c9a749b9b8b7a4
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-01-06 01:19:25 +0900

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250106/살 수 있는 물건 2/things-able-to-buy-2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d",&n);
+
+    if(n>=3000)
+    {
+        printf("book");
+    }
+    else if(n>=1000)
+    {
+        printf("mask");
+    }
+    else if(n>=500)
+    {
+        printf("pen");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
