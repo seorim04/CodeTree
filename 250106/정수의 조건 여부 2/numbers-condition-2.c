a2aed715213b912bcbcab567c9030a154b4e0208
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-01-06 01:28:47 +0900

    Title: 정수의 조건 여부 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250106/정수의 조건 여부 2/numbers-condition-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d",&a);
+
+    if(a==5)
+    {
+        printf("A");
+    }
+    else if(a%2==0)
+    {
+        printf("B");
+    }
+    else
+    {
+        printf("");
+    }
+    return 0;
+}
