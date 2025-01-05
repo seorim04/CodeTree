3c8b2787224802a4c1fb662f2239d156183540b1
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-01-06 01:47:45 +0900

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250106/3 또는 5의 배수/multiples-of-3-or-5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d",&a);
+
+    if(a%3==0)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    if(a%5==0)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    
+    return 0;
+}
