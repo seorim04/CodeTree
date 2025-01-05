cce5752034bce5f9168df16d1cf3a635266a86ce
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-01-06 01:16:28 +0900

    Title: 알파벳에 따른 평가, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250106/알파벳에 따른 평가/evaluation-by-alphabet.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char a;
+    scanf("%c", &a);
+
+    if(c=='S')
+    {
+        printf("Superior");
+    }
+    else if(c=='A')
+    {
+        printf("Excellent");
+    }
+    else if(c=='B')
+    {
+        printf("good");
+    }
+    else if(c=='C')
+    {
+        printf("Usually");
+    }
+    else if(c=='D')
+    {
+        printf("Effort");
+    }
+    else
+    {
+        printf("Failure");
+    }
+    return 0;
+}
