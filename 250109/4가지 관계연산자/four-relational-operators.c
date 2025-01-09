76f322233882d042fa31569ed25ea721432a4c42
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-01-09 14:33:32 +0900

    Title: 4가지 관계연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250109/4가지 관계연산자/four-relational-operators.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    scanf("%d %d",&a,&b);
+
+    printf("%d\n",a>=b);
+    printf("%d\n", a>b);
+    printf("%d\n", b>=a);
+    printf("%d\n", b>a);
+    return 0;
+}
