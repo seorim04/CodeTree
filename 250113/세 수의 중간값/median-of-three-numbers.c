0547130b0787f8a8593e81f63843526c11c46283
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-01-13 22:53:49 +0900

    Title: 세 수의 중간값, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250113/세 수의 중간값/median-of-three-numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    scanf("%d %d %d", &a,&b,&c);
+
+    if(b>a)
+    {
+        if(b<c)
+        {
+            printf("1");
+        }
+        else
+        {
+            printf("0");
+        }
+    }
+    return 0;
+}
