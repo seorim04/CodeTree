302360592feb2fc68bc9a4db1c21381df9e6360d
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-01-13 23:36:41 +0900

    Title: 굉장한 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250113/굉장한 수/amazing-number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+
+    if(n%2==1 && n%3==0 || n%2==0 && n%5==0)
+    {
+        printf("true");
+    }
+    else
+    {
+        printf("false");
+    }
+    return 0;
+}
