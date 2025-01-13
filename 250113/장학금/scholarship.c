74750d8ed39666b91b72367cafa513c41a1e2368
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-01-13 23:49:01 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250113/장학금/scholarship.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int mid,fin;
+    scanf("%d %d", &mid,&fin);
+
+    if(mid>=90)
+    {
+        if(fin>=95)
+        {
+            printf("100000");
+        }
+        else if(fin>=90)
+        {
+            printf("50000");
+        }
+        else
+        {
+            printf("0");
+        }
+    }
+    else
+    {
+        printf("0");
+    }
+    return 0;
+}
