f8a7a0d47bc5b35b1ff307caea5d5a977467965e
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-01-14 00:28:57 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250114/두 사람/two-person.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    char c,d;
+    scanf("%d %c",&a,&c);
+    scanf("%d %c",&b,&d);
+
+    if(a>=19 && c=='M' || b>=19 && d=='M')
+    {
+        printf("1");
+    }
+    return 0;
+}
