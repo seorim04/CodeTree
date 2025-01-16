d0ca0cbb22d456a1bdcfa4c85ddd445079fca489
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-01-16 21:04:51 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250116/코로나 메뉴얼/covid-manual.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a_fever,b_fever, c_fever;
+    char ac,bc,cc;
+    
+    scanf("%c %d\n",&ac,&a_fever);
+    scanf("%c %d\n",&bc,&b_fever);
+    scanf("%c %d",&cc,&c_fever);
+
+    if(a_fever>=37 && ac=='Y' || ac=='N')
+    {
+        if(b_fever>=37 && bc=='Y' || bc=='n')
+        {
+            printf("E");
+        }
+        else if (c_fever>=37 && cc=='Y' || cc=='N')
+        {
+            printf("E");
+        }
+        else
+        {
+            printf("N");
+        }
+    }
+    else
+    {
+        printf("N");
+    }
+
+    return 0;
+}
