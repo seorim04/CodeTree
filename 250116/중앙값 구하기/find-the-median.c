e7aad651b46874769fac216673c73fe242419a90
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-01-16 23:46:43 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250116/중앙값 구하기/find-the-median.c
+++ 250116/중앙값 구하기/find-the-median.c
@@ -5,11 +5,18 @@ int main() {
     int a,b,c;
     scanf("%d %d %d",&a,&b,&c);
 
-    if(a<b && b<c)
-        printf("%d",b);
-    if(b<a && a<c)
-        printf("%d", a);
-    if(b<c && c<a)
-        printf("%d", c);
+    if(a<b)
+    {
+        if(b<c)
+            printf("%d",b);
+        else
+            printf("%d", c);
+    }
+    else if(c>a)
+        printf("%d",a);
+    else if(b>c)
+        printf("%d",c);
+    else 
+        printf("%d", b);
     return 0;
 }
