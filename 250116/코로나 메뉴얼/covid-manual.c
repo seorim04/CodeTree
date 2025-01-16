9499dfb48bea57d1f0c6c7845ba74026cd3d786e
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-01-16 21:26:44 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250116/코로나 메뉴얼/covid-manual.c
+++ 250116/코로나 메뉴얼/covid-manual.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
 
 int main() {
-    // Please write your code here.
-    int a_fever,b_fever, c_fever;
-    char ac,bc,cc;
+    int a_fever, b_fever, c_fever;
+    char ac, bc, cc;
     
-    scanf("%c %d\n",&ac,&a_fever);
-    scanf("%c %d\n",&bc,&b_fever);
-    scanf("%c %d",&cc,&c_fever);
+    // 입력 받기
+    scanf("%c %d\n", &ac, &a_fever);
+    scanf("%c %d\n", &bc, &b_fever);
+    scanf("%c %d", &cc, &c_fever);
 
-    if(a_fever>=37 && ac=='Y' || ac=='N')
-    {
-        if(b_fever>=37 && bc=='Y' || bc=='n')
-        {
-            if (c_fever>=37 && cc=='Y' || cc=='N')
-                printf("E");
-        }
-        else if (c_fever>=37 && cc=='Y' || cc=='N')
-        {
+    // 각 사람의 상태 검사
+    if (a_fever >= 37 && ac == 'Y') {
+        if (b_fever >= 37 && bc == 'Y') {
             printf("E");
+        } else {
+            if (c_fever >= 37 && cc == 'Y') {
+                printf("E");
+            } else {
+                printf("N");
+            }
         }
-        else
-        {
+    } else {
+        if (b_fever >= 37 && bc == 'Y') {
+            if (c_fever >= 37 && cc == 'Y') {
+                printf("E");
+            } else {
+                printf("N");
+            }
+        } else {
             printf("N");
         }
-    }
-    else
-    {
-        printf("N");
     }
 
     return 0;
