c0dba6ad11741d7fb75d8e0b4caff6c0c4839ffa
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-01-17 01:16:37 +0900

    Title: 홀수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250117/홀수만 출력 2/output-only-odd-2.c
+++ 250117/홀수만 출력 2/output-only-odd-2.c
@@ -2,11 +2,12 @@
 
 int main() {
     int a, b;
-    scanf("%d %d", &a, &b);
+    scanf("%d %d", &b, &a);
 
-        for (int i = b; i >= a; i -= 2) {
-            printf("%d ", i);
-        }
+    for (int i = b; i >= a; i -= 2) 
+    {
+        printf("%d ", i);
+    }
 
 
     return 0;
