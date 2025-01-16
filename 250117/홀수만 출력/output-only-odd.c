d93ea921b82e33e514c1336305874fd409041147
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-01-17 00:46:52 +0900

    Title: 홀수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250117/홀수만 출력/output-only-odd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    scanf("%d %d",&a,&b);
+
+    for(int i=a; i<=b; i+=2)
+    {
+        printf("%d ",i);
+    }
+    return 0;
+}
