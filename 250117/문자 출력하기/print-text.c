9f47fa7ec2b63af433b03eadf92113a59b35aa97
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-01-17 14:54:13 +0900

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250117/문자 출력하기/print-text.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char a;
+    scanf("%c",&a);
+
+    for(int i=0;i<8;i++)
+    {
+        printf("%c",a);
+    }
+    return 0;
+}
