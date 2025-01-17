5c4fd1a8bd47c669a18acf616179244ce2292541
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-01-17 16:29:50 +0900

    Title: 숫자의 증감, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250117/숫자의 증감/increasing-and-decreasing-numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char c;
+    int n;
+    scanf("%c %d", &c,&n);
+
+    if(c=='A')
+    {
+        for(int i=1; i<=n; i++)
+        {
+            printf("%d ",i);
+        }
+    }
+    else if(c=='D')
+    {
+        for(int i=n; i>=1; i--)
+        {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
