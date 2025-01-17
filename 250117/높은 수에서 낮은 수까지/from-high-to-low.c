e89ff4e5edfac60240e94ff36b7bf88517357f36
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-01-17 16:38:20 +0900

    Title: 높은 수에서 낮은 수까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250117/높은 수에서 낮은 수까지/from-high-to-low.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    scanf("%d %d", &a,&b);
+    if(a>b)
+    {
+        for(int i=a;i>=b;i--)
+        {
+            printf("%d ",i);
+        }
+    }
+    else 
+    {
+        for(int i=b;i>=a;i--)
+            printf("%d ", i);
+    }
+    return 0;
+}
