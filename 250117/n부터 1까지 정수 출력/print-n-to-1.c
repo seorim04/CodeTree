cddeb5399a1789f832468267c8fb140aeeab5601
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-01-17 14:49:31 +0900

    Title: n부터 1까지 정수 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250117/n부터 1까지 정수 출력/print-n-to-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d", &n);
+
+    while(n>=1)
+    {
+        printf("%d ",n);
+        n--;
+    }
+    return 0;
+}
