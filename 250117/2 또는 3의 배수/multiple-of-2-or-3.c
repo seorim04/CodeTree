de6e3439c02b285768179deef6c6286c9c9c75b2
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-01-17 16:51:42 +0900

    Title: 2 또는 3의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250117/2 또는 3의 배수/multiple-of-2-or-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d",&n);
+
+    for(int i=1; i<=n; i++)
+    {
+        if(i%2==0 || i%3==0)
+            printf("1 ");
+        else
+            printf("0 ");
+    }
+    return 0;
+}
