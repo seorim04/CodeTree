9a21e37a3b8f9d9c8a519c95b2ae5493795fb9ec
seorim04 <150996704+seorim04@users.noreply.github.com>
2025-02-06 19:38:45 +0900

    Title: a부터 b까지의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250206/a부터 b까지의 합/sum-from-a-to-b.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    int sum=0;
+    scanf("%d %d", &a,&b);
+
+    for (int i=a; i<=b; i++)
+    {
+        sum+=i;
+    }
+    printf("%d", sum);
+    return 0;
+}
